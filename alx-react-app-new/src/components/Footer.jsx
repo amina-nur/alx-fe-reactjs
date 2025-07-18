7d43f1d3c6a45b1d12ea3b7490f144cb66007c4b
amina-nur <aminamumin606@gmail.com>
2025-07-18 13:17:36 +0300

    Add alx-react-app-new with inline style

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer style={{textAlign:'center', backgroundColor: '#333', color: 'white', padding: '10px'}}>
+      <p style={{ margin: 0 }}>
+      © 2023 City Lovers
+    </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+      <p style={{ fontSize: '18px', lineHeight: '1.5' }}>
+      I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
