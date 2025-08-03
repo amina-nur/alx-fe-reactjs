c7ca86b320b3731d81a202ba89fbeb3bed7a4355
amina-nur <aminamumin606@gmail.com>
2025-08-03 12:23:40 +0300

    Add advanced search features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
-// Function to fetch user data from GitHub API
-async function fetchUserData(username){
-    const response = await axios.get('https://api.github.com/users/' + username);
-    return response.data;
-    
-}
-export { fetchUserData };
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/search/users';
+
+// Fetch users with optional advanced filters (username, location, repos)
+const fetchUsers = async (username, location, minRepos) => {
+  // Build query string
+  let query = `${username} in:login`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}?q=${encodeURIComponent(query)}`);
+  return response.data.items; // Return array of users
+};
+
+
+export { fetchUsers };
