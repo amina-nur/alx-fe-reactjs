6984803ea49245259e1e2281d69912260c9ab01c
amina-nur <aminamumin606@gmail.com>
2025-08-03 11:18:28 +0300

    Add Github User Search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React  from "react";
+
+function App (){
+  return(
+  <div style ={{ textAlign: "center", padding: '1rem', fontFamily: 'Arial'}}>
+    <h1>Github User Search</h1>
+    <p>Start searching for github users</p>
+  </div>
+  );
+}
+
+export default App;
