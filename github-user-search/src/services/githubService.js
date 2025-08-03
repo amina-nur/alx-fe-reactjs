57b49f645e30cb705846597ae2e0be7922db412f
amina-nur <aminamumin606@gmail.com>
2025-08-03 12:54:16 +0300

    Add advanced API integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users';
+const BASE_URL = 'https://api.github.com/search/users?q';
 
 // Fetch users with optional advanced filters (username, location, repos)
 const fetchUsers = async (username, location, minRepos) => {
