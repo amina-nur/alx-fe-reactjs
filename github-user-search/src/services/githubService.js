53d8519996ccc2d8f8606bca5faeb1e05955fadb
amina-nur <aminamumin606@gmail.com>
2025-08-03 13:19:48 +0300

    Fix: added missing export for fetchUsersAdvanced

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,15 +3,19 @@ import axios from 'axios';
 const BASE_URL = 'https://api.github.com/search/users?q';
 
 // Fetch users with optional advanced filters (username, location, repos)
-const fetchUsers = async (username, location, minRepos) => {
-  // Build query string
+const fetchUsersAdvanced = async (username, location, minRepos) => {
   let query = `${username} in:login`;
   if (location) query += ` location:${location}`;
   if (minRepos) query += ` repos:>=${minRepos}`;
 
   const response = await axios.get(`${BASE_URL}?q=${encodeURIComponent(query)}`);
-  return response.data.items; // Return array of users
+  return response.data.items;
 };
 
+// Fetch user data by username (basic info)
+const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
 
-export { fetchUsers };
+export { fetchUsersAdvanced, fetchUserData };
