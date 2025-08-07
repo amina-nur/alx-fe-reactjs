7532c66587414cbf6918876caab2be6ae5d31276
amina-nur <aminamumin606@gmail.com>
2025-08-07 16:09:59 +0300

    :Fix: correct text hover color to blue

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img src="https://via.placeholder.com/150" 
       alt="User" 
       className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full w-36 h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"/>
-      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-red-500">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
         John Doe</h1>
       <p className="sm:text-sm md:text-base text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new technologies.</p>
