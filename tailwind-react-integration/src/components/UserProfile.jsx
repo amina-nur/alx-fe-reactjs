dc9615ff1db55af9607ba3fd048fee64de671005
amina-nur <aminamumin606@gmail.com>
2025-08-07 15:49:50 +0300

    Made userprofile component responsive with tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-200 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs max-w-sm mx-auto my-200 rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" 
       alt="User" 
-      className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">
+      className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full w-36 h-36 mx-auto"/>
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">
         John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <p className="sm:text-sm md:text-base text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
