57be23aa0435b40c36085f729fa5ec9d598a2916
amina-nur <aminamumin606@gmail.com>
2025-08-17 19:01:49 +0300

    feat: build responsive Home Page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,10 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
     <h1 className="text-3xl font-bold text-blue-500">
       Recipe Sharing Platform 
+      <HomePage />
     </h1>
   );
 }
